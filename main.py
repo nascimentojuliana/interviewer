63c9af0535dfb45deb400ae37d840a0fd65ded28
Juliana Nascimento Silva <juliana.silva@sulamerica.com.br>
2021-09-30 17:36:11 -0300

    remove autenticação

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ import random
 import gcsfs
 import markdown
 import pathlib
-import asyncio
 import pandas as pd
 import numpy as np
 from PIL import Image
@@ -13,104 +12,14 @@ import streamlit as st
 from utils import utils
 import pydata_google_auth
 from seletor import Seletor
-from io import BytesIO
-from io import StringIO
+from io import BytesIO, StringIO
 from google.cloud import storage
 from google.oauth2 import service_account
 from IPython.core.display import display,HTML
-from httpx_oauth.clients.google import GoogleOAuth2
-
-import asyncio
-
-from session_state import get
-from httpx_oauth.clients.google import GoogleOAuth2
 
 
 project_id = st.secrets["project_id"]
 bucket_name = st.secrets["bucket_name"]
-
-
-client_id = st.secrets['client_id'] 
-client_secret = st.secrets['client_secret'] 
-redirect_uri = st.secrets['redirect_uri']
-
-
-async def write_authorization_url(client,
-                                  redirect_uri):
-    authorization_url = await client.get_authorization_url(
-        redirect_uri,
-        scope=["profile", "email"],
-        extras_params={"access_type": "offline"},
-    )
-    return authorization_url
-
-
-async def write_access_token(client,
-                             redirect_uri,
-                             code):
-    token = await client.get_access_token(code, redirect_uri)
-    return token
-
-
-async def get_email(client,
-                    token):
-    user_id, user_email = await client.get_id_email(token)
-    return user_id, user_email
-
-
-def main(user_id, user_email):
-    st.write(f"You're logged in as {user_email}")
-
-client = GoogleOAuth2(client_id, client_secret)
-authorization_url = asyncio.run(
-    write_authorization_url(client=client,
-                            redirect_uri=redirect_uri)
-)
-
-session_state = get(token=None)
-if session_state.token is None:
-    try:
-        code = st.experimental_get_query_params()['code']
-    except:
-        st.write(f'''<h1>
-            Please login using this <a target="_self"
-            href="{authorization_url}">url</a></h1>''',
-                 unsafe_allow_html=True)
-    else:
-        # Verify token is correct:
-        try:
-            token = asyncio.run(
-                write_access_token(client=client,
-                                   redirect_uri=redirect_uri,
-                                   code=code))
-        except:
-            st.write(f'''<h1>
-                This account is not allowed or page was refreshed.
-                Please try again: <a target="_self"
-                href="{authorization_url}">url</a></h1>''',
-                     unsafe_allow_html=True)
-        else:
-            # Check if token has expired:
-            if token.is_expired():
-                if token.is_expired():
-                    st.write(f'''<h1>
-                    Login session has ended,
-                    please <a target="_self" href="{authorization_url}">
-                    login</a> again.</h1>
-                    ''')
-            else:
-                session_state.token = token
-                user_id, user_email = asyncio.run(
-                    get_email(client=client,
-                              token=token['access_token'])
-                )
-                session_state.user_id = user_id
-                session_state.user_email = user_email
-                main(user_id=session_state.user_id,
-                     user_email=session_state.user_email)
-else:
-    main(user_id=session_state.user_id,
-         user_email=session_state.user_email)
 
 #account = 'sas-sandbox-advanced-analytics-7b8b0505d8dd.json'
 
@@ -119,9 +28,9 @@ else:
 #credentials = get_credentials(project_id,bucket_name,service_account)
 path = 'gs://interviewer/questoes.csv'
 
-#credentials = pydata_google_auth.get_user_credentials(['https://www.googleapis.com/auth/cloud-platform'],)
+credentials = pydata_google_auth.get_user_credentials(['https://www.googleapis.com/auth/cloud-platform'],)
 
-gcs_client = storage.Client(project=project_id)
+gcs_client = storage.Client(project=project_id, credentials=credentials)
 
 bucket = gcs_client.get_bucket(bucket_name)
 
@@ -160,8 +69,8 @@ cargo = st.sidebar.selectbox(
 #path
 #path
 
-#fs = gcsfs.GCSFileSystem(project=project_id, token=token)
-#if fs:
+# fs = gcsfs.GCSFileSystem(project=project_id, token=path)
+# if fs:
 # 	with fs.open('interviewer/questoes.csv') as f:
 # 		df = pd.read_csv(f)
 
